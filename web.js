1345e0a9f2e04dc61fa1e2ce5ddb7576c07a751d
Aprameya Paduthonse <aprameya.paduthonse@gmail.com>
2013-07-11 23:37:57 +0000

    4

--- web.js
+++ web.js
@@ -7,8 +7,9 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  var fileContents = fs.readFile('./index.html');
-  response.send(fileContents.toString());
+  //var fileContents = fs.readFile('./index.html');
+  //response.send(fileContents.toString());
+  response.send(fs.readFileSync('./index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
